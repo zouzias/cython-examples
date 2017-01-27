13beed562901c801d5093f0eaa93a756f72a0254
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2017-01-27 06:53:41 +0100

    remove unsued import

--- scipy-sparse-frob/tests.py
+++ scipy-sparse-frob/tests.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import numpy as np
 import sparsefrob
 import math
 from scipy.sparse import csr_matrix
@@ -16,13 +15,16 @@ def testSparseMatrix(A):
     expectedNorm = norm(A.data)
     assert math.fabs( frob - expectedNorm) < TOL
 
+
 def test_csr_norm():
     A = csr_matrix([[1.0, 2.0, 0.0], [0.0, 0.0, 3.0], [4.0, 0.0, 5.0]])
     testSparseMatrix(A)
 
+
 def test_csc_norm():
     A = csc_matrix([[1.0, 2.0, 0.0], [0.0, 0.0, 3.0], [4.0, 0.0, 5.0]])
     testSparseMatrix(A)
+
 
 def test_coo_norm():
     A = coo_matrix([[1.0, 2.0, 0.0], [0.0, 0.0, 3.0], [4.0, 0.0, 5.0]])
